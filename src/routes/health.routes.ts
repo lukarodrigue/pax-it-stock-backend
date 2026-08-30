6ccce13c972fc6c1bf505aa7d8578c3c862d969a
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2026-08-30 15:45:33 -0400

    chore: router test

--- src/routes/health.routes.ts
+++ src/routes/health.routes.ts
@@ -1,10 +1,24 @@
 import { Router } from "express";
 import { HealthController } from "../controllers/health.controller.js";
+import { validate } from "../middlewares/validation.middleware.js";
+import { healthSchema } from "../schemas/health.schema.js";
 
 const router = Router();
 
 const healthController = new HealthController();
 
 router.get("/", healthController.getStatus);
+
+router.post(
+  "/test",
+  validate(healthSchema),
+  (req, res) => {
+    return res.status(200).json({
+      success: true,
+      message: "Dados válidos",
+      data: req.body
+    });
+  }
+);
 
 export default router;
